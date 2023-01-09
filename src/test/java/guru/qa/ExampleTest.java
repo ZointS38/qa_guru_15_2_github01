af4d148c872c67b773f213640fdc082c110ebea3
ZointS38 <artfedoz@gmail.com>
2023-01-09 23:25:16 +0100

    change logic, add random

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -3,11 +3,13 @@ package guru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest()  {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
