80d42f9679cd7981e0889e5a0652f6e3d04ed227
ZointS38 <artfedoz@gmail.com>
2023-01-09 23:18:09 +0100

    change logic

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -7,11 +7,11 @@ public class ExampleTest {
 
     @Test
     void exampleTest()  {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(2 >= 1);
     }
 
     @Test
     void exampleTest1()  {
-        Assertions.assertTrue(2 + 2 == 4);
+        Assertions.assertTrue(3 + 2 == 5);
     }
 }
